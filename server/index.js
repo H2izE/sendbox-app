4f87573fc8982ecaa8196e23eab7ee4ffdb240df
sjjang1214 <87017435+H2izE@users.noreply.github.com>
2021-11-09 19:59:52 +0900

    add products api

--- server/index.js
+++ server/index.js
@@ -2,10 +2,21 @@ var express = require('express');
 var app = express();
 const PORT = 8080;
 
+const products = [
+    {
+        name: 'aa',
+        price: 30000,
+        year: 2002,
+    },
+]
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('hello world');
 });
+
+app.get('/products', function (req, res) {
+    res.send(products);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
