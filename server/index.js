5a856264dfb5ea1903c86e16983cd1912b801656
sjjang1214 <87017435+H2izE@users.noreply.github.com>
2021-11-27 14:31:41 +0900

    server CRUD

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var app = express();
-const PORT = 8090;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 8080;
 
 const products = [
     {
@@ -29,6 +32,49 @@ app.get('/', function (req, res) {
 
 app.get('/products', function (req, res) {
     res.send(products);
+})
+
+app.get('/products/:id', function (req, res) {
+    console.log(req.params);
+    const id = req.params.id;
+    if (products[id]) {
+        res.send(products[`${req.params.id}`]);
+    } else {
+        res.send({ message: `id ${id} is not exisited` })
+    }
+
+});
+
+app.post('/products', function (req, res) {
+    const { name, price, year } = req.body;
+    const id = products.length;
+    const newProduct = {
+        id, name, price, year
+    };
+    products.push(newProduct);
+    res.send({ message: `new product with id:${id} is created` });
+
+})
+
+app.put('/products/:id', function (req, res) {
+    const id = req.params.id;
+    const { name, price, year } = req.body;
+    if (products[id]) {
+        products[id] = { id, name, price, year };
+        res.send({ message: `id ${id} is updated` });
+    } else {
+        res.send({ message: `id ${id} is not existed` });
+    }
+})
+
+app.delete('/products/:id', function (req, res) {
+    const id = req.params.id;
+    if (products[id]) {
+        products.splice(id, 1);
+        res.send({ message: `id ${id} is deleted` });
+    } else {
+        res.send({ message: `id ${id} is not existed` });
+    }
 })
 
 app.listen(PORT, () => {
--- src/Products.js
+++ src/Products.js
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react"
 export const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:8090/products')
+        fetch('http://localhost:8080/products')
             .then(res => res.json())
             .then(data => setProducts(data))
             .catch(err => console.error(err))
-    })
+    }, [])
 
     return (
         <div>
