46b2ed9a4d24c36d803cda54afd118545dfa5c73
sjjang1214 <87017435+H2izE@users.noreply.github.com>
2021-12-21 20:47:57 +0900

    Logout and NavigationBar 추가

---
+++ client/src/Logout.js
@@ -0,0 +1,9 @@
+const Logout = (setUserInfo) => {
+    return (
+        <div>
+            <button onClick={() => { setUserInfo(null) }}>Logout</button>
+        </div>
+    )
+}
+
+export default Logout
